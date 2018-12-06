681bbfc413c72b31602fb18c154bd27f299fd340
Laurent Hemeryck <hemie143@gmail.com>
2018-12-06 17:31:10 +0100

    Day03 part 2 test

--- day03.py
+++ day03.py
@@ -1,6 +1,6 @@
 import re
 
-def fabric_overlap(claims):
+def load_grid(claims):
     grid = {}
     for c in claims:
         r = re.match('#([0-9]+) @ ([0-9]+),([0-9]+): ([0-9]+)x([0-9]+)', c)
@@ -11,17 +11,31 @@ def fabric_overlap(claims):
                     elves = grid.get((i, j), set())
                     elves.add(elf)
                     grid[(i, j)] = elves
+    return grid
 
+def fabric_overlap(grid):
     overlap = 0
     for elves in grid.values():
         if len(elves) > 1:
             overlap += 1
     return overlap
 
+def test(claims):
+    for k, cell in grid.items():
+        print(f'{k}:{cell}')
+        # pass
+    return
+
 
 if __name__ == '__main__':
     with open('input03.txt') as f:
         claims = f.readlines()
-    overlap = fabric_overlap(claims)
+    grid = load_grid(claims)
+    overlap = fabric_overlap(grid)
+    test(claims)
     print(f'Day 03 - Part 1 - Answer: {overlap}')
+
+    print(load_grid(['#1 @ 1,3: 4x4',
+                                 '#2 @ 3,1: 4x4',
+                                 '#3 @ 5,5: 2x2']))
 
