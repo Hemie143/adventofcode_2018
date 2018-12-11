d0879a73394a48e7b85bd17e3ff0ad54a5fff590
hemie143@gmail.com <1tb8NQ6PvKBw>
2018-12-11 23:16:35 +0100

    Day06 load

--- day06.py
+++ day06.py
@@ -1,8 +1,18 @@
 
+def load_coords(data):
+    coords = []
+    for d in data:
+        r = re.match(r'[0-9]+, [0-9]+', d)
+        x, y = r.group(1, 2)
+        coords.append(x, y)
+    return coords
+
+
 
 if __name__ == '__main__':
     with open('input06.txt') as f:
-        polymer = f.read()
+        data = f.readlines()
+    coords = load_coords(data)
     # polymer_reacted = react(polymer)
     # print(f'Day 05 - Part 1 - Answer: {len(polymer_reacted)}')
     # print(f'Day 05 - Part 2 - Answer: {optimize(polymer)}')
