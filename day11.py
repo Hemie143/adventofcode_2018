2ab77a0074cb45641f7ce22c4998189b83df6eb0
hemie143 <hemie143@gmail.com>
2019-01-10 09:00:34 +0100

    Day 11 - part 2

--- day11.py
+++ day11.py
@@ -1,34 +1,24 @@
 from collections import defaultdict
 
 
-def cell_pwr(x, y, serial):
-    rackId = x + 10
-    power = (int(((rackId * y + serial) * rackId / 100)) % 10) - 5
-    return power
-
-
-def generate_grid(grid_size, serial):
+def generate_summed_area(grid_size, serial):
     grid_power = defaultdict(int)
     for x in range(1, grid_size + 1):
         rackId = x + 10
         for y in range(1, grid_size + 1):
-            grid_power[(x, y)] = (int(((rackId * y + serial) * rackId / 100)) % 10) - 5
+            p = (int(((rackId * y + serial) * rackId / 100)) % 10) - 5
+            # https://en.wikipedia.org/wiki/Summed-area_table
+            grid_power[(x, y)] = p + grid_power[(x, y-1)] + grid_power[(x-1, y)] - grid_power[(x-1, y-1)]
     return grid_power
-
-'''
-def square_pwr(grid_power, x, y, select_size):
-    return sum([grid_power[x + i][y + j] for i in range(select_size) for j in range(select_size)])
-'''
-
-def square_pwr(grid_power, x, y, select_size):
-    return sum([grid_power[(x + i, y + j)] for i in range(select_size) for j in range(select_size)])
 
 
 def select_square(grid_power, grid_size, select_size):
     square_powers = []
     for x in range(1, grid_size - select_size + 1):
         for y in range(1, grid_size - select_size + 1):
-            sp = square_pwr(grid_power, x, y, select_size)
+            x0, y0 = x-1, y-1
+            x1, y1 = x+select_size-1, y+select_size-1
+            sp = grid_power[(x1, y1)] + grid_power[(x0, y0)] - grid_power[(x1, y0)] - grid_power[(x0, y1)]
             square_powers.append((sp, x, y))
     return max(square_powers)
 
@@ -36,33 +26,11 @@ def select_squaresize(grid_power, grid_size):
     square_powers = []
     # https://en.wikipedia.org/wiki/Summed-area_table
     for size in range(1, grid_size):
-        print(f'size: {size}')
+        # print(f'size: {size}')
         sp = select_square(grid_power, grid_size, size)
         square_powers.append((sp, size))
     return max(square_powers)
 
-def select_cell(grid_size, select_size, serial):
-    grid_power = [[0] * grid_size for _ in range(grid_size)]
-    for x in range(1, grid_size - select_size):
-        for y in range(1, grid_size - select_size):
-            grid_power[x][y] = cell_pwr(x, y, serial)
-    max_cell_power = sum([grid_power[1+i][1+j] for i in range(select_size) for j in range(select_size)])
-    for x in range(1, grid_size - select_size):
-        for y in range(1, grid_size - select_size):
-            cell_power = square_pwr(grid_power, x, y, select_size)
-            if cell_power > max_cell_power:
-                max_cell_power = cell_power
-                cell_x, cell_y = x, y
-    return cell_x, cell_y, max_cell_power
-
-def select_cellsize(grid_size, serial):
-    max_cell_power = 0
-    for size in range(1, grid_size):
-        x, y, cell_power = select_cell(grid_size, size, serial)
-        print('size: {} - power: {}'.format(size, cell_power))
-        if cell_power > max_cell_power:
-            best_x, best_y, max_cell_power = x, y, cell_power
-    return x, y, size
 
 if __name__ == '__main__':
     with open('input11.txt') as f:
@@ -70,9 +38,9 @@ if __name__ == '__main__':
 
     grid_size = 300
     select_size = 3
-    grid_power = generate_grid(grid_size, serial)
+    grid_power = generate_summed_area(grid_size, serial)
     _, x, y = select_square(grid_power, grid_size, select_size)
-    print(f'Day 11 - Part 1 - Answer: {x}, {y}')
-    result = select_squaresize(grid_power, grid_size)
-    print(f'Day 11 - Part 2 - Answer: {result}')
+    print(f'Day 11 - Part 1 - Answer: {x},{y}')
+    sp, size = select_squaresize(grid_power, grid_size)
+    print(f'Day 11 - Part 2 - Answer: {sp[1]},{sp[2]},{size}')  # 236,252,12
 
--- tests/test11.py
+++ tests/test11.py
@@ -4,36 +4,13 @@ from adventofcode_2018 import day11
 
 @pytest.fixture
 def grid_power():
-    return day11.generate_grid(300, 8)
+    return day11.generate_summed_area(300, 8)
 
 
 def test_day11(grid_power):
 
-    assert grid_power[(3, 5)] == 4
-    assert day11.generate_grid(300, 57)[(122, 79)] == -5
-    assert day11.generate_grid(300, 39)[(217, 196)] == 0
-    assert day11.generate_grid(300, 71)[(101, 153)] == 4
+    assert day11.select_square(day11.generate_summed_area(300, 18), 300, 3) == (29, 33, 45)
+    assert day11.select_square(day11.generate_summed_area(300, 42), 300, 3) == (30, 21, 61)
 
-    assert day11.select_square(day11.generate_grid(300, 18), 300, 3) == (29, 33, 45)
-    assert day11.select_square(day11.generate_grid(300, 42), 300, 3) == (30, 21, 61)
-    '''
-    assert day11.cell_pwr(3, 5, 8) == 4
-    assert day11.cell_pwr(122, 79, 57) == -5
-    assert day11.cell_pwr(217, 196, 39) == 0
-    assert day11.cell_pwr(101, 153, 71) == 4
-
-    assert day11.cell_pwr(33, 45, 18) == 4
-    assert day11.cell_pwr(34, 45, 18) == 4
-    assert day11.cell_pwr(35, 45, 18) == 4
-    assert day11.cell_pwr(33, 46, 18) == 3
-    assert day11.cell_pwr(34, 46, 18) == 3
-    assert day11.cell_pwr(35, 46, 18) == 4
-    assert day11.cell_pwr(33, 47, 18) == 1
-    assert day11.cell_pwr(34, 47, 18) == 2
-    assert day11.cell_pwr(35, 47, 18) == 4
-
-    assert day11.select_cell(51, 3, 18) == (33, 45, 29)
-    assert day11.select_cell(70, 3, 42) == (21, 61, 30)
-
-    assert day11.select_cellsize(300, 18) == (90, 269, 16)
-    '''
+    assert day11.select_squaresize(day11.generate_summed_area(300, 18), 300) == ((113, 90, 269), 16)
+    assert day11.select_squaresize(day11.generate_summed_area(300, 42), 300) == ((119, 232, 251), 12)
